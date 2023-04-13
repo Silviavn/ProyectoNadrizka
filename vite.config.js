af8fc3c674c062e59fd7f8b5f535e007b3a72e86
silvia <silviavergara.14sa@gmail.com>
2023-04-12 21:40:17 -0400

    prueba producción

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 export default {
   root: path.resolve(__dirname),
-  publicDir:"html",
+  publicDir:"docs",
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
