349c870b840904cb81b7718b3b8452d47f6895ad
silvia <silviavergara.14sa@gmail.com>
2023-05-13 12:06:56 -0400

    actualizacion vite json pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 const path = require('path')
-
 export default {
   root: path.resolve(__dirname),
-  publicDir:"docs",
+  publicDir: "docs",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        cortes: path.resolve(__dirname, 'pages/cortes.html'),
+        lavado: path.resolve(__dirname, 'pages/lavado.html'),
+        permanente: path.resolve(__dirname, 'pages/permanente.html'),
+        coloracion: path.resolve(__dirname, 'pages/coloracion.html'),
+        peinados: path.resolve(__dirname, 'pages/peinados.html'),
+      }
+    }
+  },
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
